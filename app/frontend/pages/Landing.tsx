4542fcf360a195354bf040d52d13cf97b252a6db
alex <alexanderyi933@gmail.com>
2025-11-12 12:39:52 -0500

    add "gomi_guesser" to routes

---
+++ app/frontend/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+export default function InertiaExample() {
+  return (
+    <>
+      <Head title="Inertia + Vite Ruby + React Example" />
+      <div>
+        <h1>Landing page</h1>
+      </div>
+    </>
+  );
+}
