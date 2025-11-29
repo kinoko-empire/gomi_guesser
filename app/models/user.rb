06e3c847a29d949b64059e01e45d5aa084382776
alex <alexanderyi933@gmail.com>
2025-11-29 03:57:00 -0500

    adds "role" enum to User model so helper methods can be used

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  # implemented enum from reading this article: https://jasonchee.me/notes/enum-with-sqlite/
+  # api page for rails 8.0.0 here: https://api.rubyonrails.org/v8.0.0/classes/ActiveRecord/Enum.html
+  enum :role, { admin: "admin", standard: "standard" }, prefix: true
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
