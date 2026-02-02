884d6097268d4d45867fd09a92b91f186b9a1888
Alex Yi <87381690+alex-yi37@users.noreply.github.com>
2026-02-01 23:51:45 -0500

    remove typelizer references in ruby code (#38)

--- app/serializers/ApplicationResource.rb
+++ app/serializers/ApplicationResource.rb
@@ -1,6 +1,3 @@
 class ApplicationResource
   include Alba::Resource
-  # For Alba, we recommend using the `helper` method instead of `include`.
-  # See the documentation: https://github.com/okuramasafumi/alba/blob/main/README.md#helper
-  helper Typelizer::DSL
 end
