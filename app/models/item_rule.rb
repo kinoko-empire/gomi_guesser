e2352fc33865d67f7aab747b4fa2168b3589e7f5
alex <alexanderyi933@gmail.com>
2026-04-09 14:42:53 -0400

    adds additional info for uniqueness constraint with a scope usage in ItemRule model

--- app/models/item_rule.rb
+++ app/models/item_rule.rb
@@ -1,4 +1,5 @@
 class ItemRule < ApplicationRecord
   # from here - https://www.reddit.com/r/rails/comments/m7oypl/uniqueness_validation_on_two_columns_of_same_field/
+  # more explanation on how the uniqueness constraint works with a scope - https://boringrails.com/tips/rails-unique-scope
   validates :item_id, uniqueness: { scope: %i[municipality_id material_tag_id sorting_category_id] }
 end
