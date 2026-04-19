49f737bd842fc5b138fd5704daafb8162cb3c7f5
alex <alexanderyi933@gmail.com>
2026-04-18 23:24:17 -0400

    adds seed data for item_rules

--- app/models/item_rule.rb
+++ app/models/item_rule.rb
@@ -1,5 +1,8 @@
 class ItemRule < ApplicationRecord
   # from here - https://www.reddit.com/r/rails/comments/m7oypl/uniqueness_validation_on_two_columns_of_same_field/
   # more explanation on how the uniqueness constraint works with a scope - https://boringrails.com/tips/rails-unique-scope
-  validates :item_id, uniqueness: { scope: %i[municipality_id material_tag_id sorting_category_id] }
+  validates :item_id, uniqueness: {
+    scope: %i[municipality_id material_tag_id sorting_category_id],
+    message: "A rule must be unique for a given municipality, item, sorting category, and material tag combination"
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -40,17 +40,27 @@ def create_seed_data
   municipalities_text = File.read(Rails.root.join("db", "seed_data", "jp_municipalities.csv"))
   municipalities_csv = CSV.parse(municipalities_text, headers: true, encoding: "UTF-8")
 
+  # create mapping so an item_rule can find correct foreign keys when being created
+  item_rule_id_references = {
+    municipality_ids: {},
+    sorting_category_map: {},
+    material_tag_map: {},
+    item_map: {}
+  }
+
   if !Municipality.first
-    puts "seeding #{municipalities_csv.length} prefectures"
+    puts "seeding #{municipalities_csv.length} municipalities"
 
     municipalities_csv.each do |row|
-      Municipality.find_or_create_by(eng_name: row["eng_name"], prefecture_id: prefecture_id_map[row["prefecture_name"]]) do |c|
+      munc = Municipality.find_or_create_by(eng_name: row["eng_name"], prefecture_id: prefecture_id_map[row["prefecture_name"]]) do |c|
         c.kanji_name = row["kanji_name"]
         c.kana_name = row["kana_name"]
         c.municipality_type = row["municipality_type"]
         c.alphanumeric_eng_name = row["alphanumeric_eng_name"]
         c.info_url = row["info_url"]
       end
+
+      item_rule_id_references[:municipality_ids]["#{row["eng_name"]}-#{row["prefecture_name"]}"] = munc.id
     end
   else
     puts "municipalities already exist, skipping "
@@ -63,7 +73,9 @@ def create_seed_data
     puts "seeding sorting categories"
 
     sorting_categories_csv.each do |row|
-      SortingCategory.create(name: row["name"])
+      sc = SortingCategory.create(name: row["name"])
+
+      item_rule_id_references[:sorting_category_map][row["name"]] = sc.id
     end
   else
     puts "sorting categories already exist, skipping"
@@ -76,10 +88,48 @@ def create_seed_data
     puts "seeding material tags"
 
     material_tags_csv.each do |row|
-      MaterialTag.create(material_name: row["material_name"])
+      mt = MaterialTag.create(material_name: row["material_name"])
+
+      item_rule_id_references[:material_tag_map][row["material_name"]] = mt.id
     end
   else
     puts "material tags already exist, skipping"
+  end
+
+  items_text = File.read(Rails.root.join("db", "seed_data", "item_list.csv"))
+  items_csv = CSV.parse(items_text, headers: true, encoding: "UTF-8")
+
+  if !Item.first
+    puts "seeding items"
+
+    items_csv.each do |row|
+      item = Item.create(eng_name: row["eng_name"])
+
+      item_rule_id_references[:item_map][row["eng_name"]] = item.id
+    end
+  else
+    puts "items already exist, skipping"
+  end
+
+  item_rules_text = File.read(Rails.root.join("db", "seed_data", "item_rules.csv"))
+  item_rules_csv = CSV.parse(item_rules_text, headers: true, encoding: "UTF-8")
+
+  puts item_rule_id_references
+
+  if !ItemRule.first
+    puts "seeding item rules"
+
+    item_rules_csv.each do |row|
+      ItemRule.create(
+        municipality_id: item_rule_id_references[:municipality_ids]["#{row["municipality_name"]}-#{row["prefecture_name"]}"],
+        sorting_category_id: item_rule_id_references[:sorting_category_map][row["sorting_category"]],
+        material_tag_id: item_rule_id_references[:material_tag_map][row["material_name"]],
+        item_id: item_rule_id_references[:item_map][row["item_name"]],
+        additional_notes: row["additional_notes"]
+      )
+    end
+  else
+    puts "item rules already exist, skipping"
   end
 
   puts "seed script finished"
