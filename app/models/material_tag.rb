2ead3902cc3d9c438a54868312724cde8465b766
alex <alexanderyi933@gmail.com>
2026-04-25 17:27:59 -0400

    create model methods for displaying material_tag and sorting_category names

--- app/models/material_tag.rb
+++ app/models/material_tag.rb
@@ -1,3 +1,20 @@
 class MaterialTag < ApplicationRecord
   validates :material_name, presence: true, uniqueness: { case_sensitive: false }
+
+  def display_name_mapping
+    {
+      cardboard: "Cardboard",
+      foam: "Foam",
+      metal: "Metal",
+      organic_waste: "Organic waste",
+      paper: "Paper",
+      pet_plastic: "PET plastic",
+      plastic: "Plastic",
+      uncategorized: "Uncategorized"
+    }
+  end
+
+  def display_name
+    display_name_mapping[self.material_name.parameterize.underscore.to_sym]
+  end
 end
--- app/models/sorting_category.rb
+++ app/models/sorting_category.rb
@@ -1,3 +1,24 @@
 class SortingCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def display_name_mapping
+    {
+      recyclable_plastic: "Recyclable plastic",
+      recyclable_plastic_bottle: "Recyclable plastic bottle",
+      recyclable_glass_bottle: "Recyclable glass bottle",
+      recyclable_can: "Recyclable can",
+      recyclable_paper: "Recyclable paper",
+      combustible: "Combustible",
+      non_combustible: "Non-combustible"
+    }
+  end
+
+  def display_name
+    # got the "parameterize" method call from https://stackoverflow.com/questions/2004491/convert-string-to-symbol-able-in-ruby
+    display_name_mapping[self.name.parameterize.underscore.to_sym]
+  end
+
+  def self.get_display_name(name)
+    display_name_mapping(name.parameterize.underscore.to_sym)
+  end
 end
